093131cfe96da5435d5127d1e4437f0beb02d16f
Misato Kano <me@mirror-kt.dev>
2026-08-11 19:27:20 +0900

    ♻️  Migrate tsup to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: "esm",
-  splitting: false,
   sourcemap: true,
   clean: true,
+  dts: true,
+  target: false,
 });
